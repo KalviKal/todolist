d299fcd5f4d65365a8ede1ac7f0a382ef3861473
KalviKal <66356112+KalviKal@users.noreply.github.com>
2025-05-01 13:26:44 +0300

    Initial commit

---
+++ src/TodoMain.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import './App.css'
+
+import ItemsList from './ItemsList'
+import AddTodo from './AddTodo'
+
+function TodoMain() {
+  const [count, setCount] = useState(0)
+
+  const [items, setItems] = useState([
+    { id: 1, name: 'Õun', idDone: false, unit:'kg' },
+    { id: 2, name: 'Banaan', idDone: false, unit:'kg' },
+    { id: 3, name: 'Pirn', idDone: false, unit:'tk' },
+    { id: 4, name: 'Merevaik', idDone: false, unit:'tk' },
+    { id: 5, name: 'Kohv', idDone: false, unit:'tk' },
+    { id: 6, name: 'Tee', idDone: false, unit:'tk' },
+    { id: 7, name: 'Suhkur', idDone: false, unit:'tk' },
+    { id: 8, name: 'Jahu', idDone: false, unit:'tk' },
+    { id: 9, name: 'Sool', idDone: false, unit:'tk' },
+    { id: 10, name: 'Pasta',idDone: false, unit:'tk' }
+ 
+  ])
+
+  const addNewTodo = (newTodo) => {
+    console.log(newTodo)
+    const newItem = {
+      id: items.length + 1,
+      name: newTodo.name,
+      idDone: false,
+      unit: newTodo.unit
+    }
+    setItems([...items, newItem])
+  }
+
+  return (
+    
+    <div className='conteiner'>
+    <h1>Minu ostukorv</h1>
+    <AddTodo addNewTodo={addNewTodo}/>
+    <ItemsList items={items} />
+    </div>
+    
+  )
+}
+
+export default TodoMain
